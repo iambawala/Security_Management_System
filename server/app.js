de968f4080cdb4b878015d1bf3f7e156c414e622
iambawala <premhiprem010@gmail.com>
2026-05-07 16:46:38 +0530

    export app fix 2x

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ import  dotenv from "dotenv"
 dotenv.config()
 
 import  connectDB from "./db/config.js"
-connectDB() // Connect to MongoDB
+// connectDB() // Connect to MongoDB
 
 app.get("/health", (req,res)=> {
     res.send("Health ok, Server Running")
